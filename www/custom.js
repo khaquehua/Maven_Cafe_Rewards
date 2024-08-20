77eb30be6e66d5931480786cbde60918ac976989
khaquehua <kevinhaquehua@gmail.com>
2024-08-20 16:56:00 -0500

    Delete js don't use

--- www/custom.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.sidebar .nav-item .nav-link').forEach(function(el) {
-    if (el.textContent.includes('?')) {
-      el.addEventListener('click', function() {
-        alert('Activaste el botón de ayuda!');
-      });
-    }
-  });
-});
